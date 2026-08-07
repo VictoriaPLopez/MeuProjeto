b9ffa3a1862002a3f880bdc5a3fa6e4f7a9a513e
VICTORIA VALENTINA PRADO LOPEZ <victoria_lopez@estudante.sesisenai.org.br>
2026-08-06 21:40:11 -0300

    criacao das rotas

---
+++ index.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import express from 'express';
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res) =>{
+    res.json({
+        message: "Funcionando",
+    })
+})
+
+app.get("/teste", (req,res) =>{
+    res.json({
+        message:"Testando"
+    })
+})
+
+app.listen(port);
+
+console.log("Backend rodando!")
+
